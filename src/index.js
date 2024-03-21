065bf627ddc75f2bbbc9baa30fa9b1c7c9b1bf9e
Sara Sanchez Crespo <saritasc89@gmail.com>
2024-03-21 18:53:44 +0000

    .env and remove node modules

--- src/index.js
+++ src/index.js
@@ -1,37 +1,40 @@
 // IMPORTAR BIBLIOTECAS
-const express = require('express');
-const cors = require('cors');
-//
-const mysql = require('mysql2/promise');
-
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const mysql = require("mysql2/promise");
 
 //CREAR VARIABLES
 const app = express();
 const port = 4000;
 
-
 //CONFIGURACIÓN EXPRESS
 app.use(cors());
-app.use(express.json({limit: '25Mb'}));
-
-
+app.use(express.json({ limit: "25Mb" }));
 
 //CONFIGURACIÓN MYSQL
 
+const getConnection = async () => {
+  const connection = await mysql.createConnection({
+    host: process.env.MYSQL_HOST || "localhost",
+    user: process.env.MYSQL_USER || "root",
+    password: process.env.MYSQL_PASS,
+    database: process.env.MYSQL_SCHEMA || "recetas_db",
+  });
 
+  await connection.connect();
 
+  console.log(
+    `Conexión establecida en la base de datos (identificador=${connection.threadId})`
+  );
+};
 
 //ARRANCAR
 
 app.listen(port, () => {
   console.log(`Server has been started in <http://localhost:${port}`);
-})
-
-
+});
 
 //ENDPOINTS
 
-// app.get('/', (req, res) =>{
-//   res.send ('Holis!')
-// })
-
+getConnection()
